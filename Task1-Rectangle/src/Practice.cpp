18bdba4841405ba8d28594ec5ac1bc568ae65725
Kamrankb <thekamrankb@gmail.com>
2022-10-06 19:23:46 +0500

    Added comments for better understanding

--- Task1-Rectangle/src/Practice.cpp
+++ Task1-Rectangle/src/Practice.cpp
@@ -70,19 +70,26 @@ public:
 
 int main() {
 	Rectangle rect;
+	//get length and width of Rectangle respectively
 	rect.getLength();
 	rect.getWidth();
 
+	//Setting invalid length to check the exception
 	rect.setLength(23.22);
 
+	//set length and width of Rectangle respectively
 	rect.setWidth(10.2);
 	rect.setLength(12.2);
 
+	//Calling function to find Perimeter of Rectangle
 	rect.perimeter();
-
+	
+	//Calling function to find Area of Rectangle
 	rect.area();
 
+	//getting length and width of Rectangle respectively
 	rect.getLength();
 	rect.getWidth();
+	
 	return 0;
 }
